be77e9d3c6f816ad8f175e5bda564b1f545eb973
Kate <Katrinekatek@gmail.com>
2019-08-22 13:01:18 +0300

    [ex1-7]

--- company/test.php
+++ company/test.php
@@ -2,18 +2,11 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	"vertical_menu",
+	"bitrix:breadcrumb",
+	".default",
 	Array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "1",
-		"MENU_CACHE_GET_VARS" => array(0=>"",),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "N",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "left",
-		"USE_EXT" => "Y"
+		"PATH" => "",
+		"SITE_ID" => "s1",
+		"START_FROM" => "0"
 	)
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
