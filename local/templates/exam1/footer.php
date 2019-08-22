ffc8e0156e7600c0f8ec5a6fc1bdbc42f091406d
Kate <Katrinekatek@gmail.com>
2019-08-22 14:21:18 +0300

    Auto stash before merge of "master" and "origin/master"

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -114,6 +114,7 @@
 <div class="item">
 <div class="title-block">О магазине</div>
 <?$APPLICATION->IncludeComponent(
+<<<<<<< Updated upstream
 	"bitrix:menu",
 	"vertical_multilevel",
 	Array(
@@ -123,12 +124,29 @@
 		"DELAY" => "N",
 		"MAX_LEVEL" => "1",
 		"MENU_CACHE_GET_VARS" => array(),
+=======
+	"bitrix:menu", 
+	"bottom", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"COMPONENT_TEMPLATE" => "bottom",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+>>>>>>> Stashed changes
 		"MENU_CACHE_TIME" => "3600",
 		"MENU_CACHE_TYPE" => "N",
 		"MENU_CACHE_USE_GROUPS" => "Y",
 		"ROOT_MENU_TYPE" => "bottom",
 		"USE_EXT" => "N"
+<<<<<<< Updated upstream
 	)
+=======
+	),
+	false
+>>>>>>> Stashed changes
 );?>
 </div>
 <div class="item">
